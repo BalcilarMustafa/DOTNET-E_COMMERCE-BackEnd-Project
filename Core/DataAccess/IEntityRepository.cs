d950ee1daed33d5410bee4e31fd2258eb3b6d86a
BalcilarMustafa <mbalcilar22@gmail.com>
2021-07-08 22:39:46 +0200

    E-Commerce Backend Project

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    // generic constraint
+    // class : reference type
+    // It can be an IEntity or an object implements IEntity
+    // It must be instantiated. (new())
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    // A class implements IEntity is a database table.
+    public interface IEntity
+    {
+    }
+}
